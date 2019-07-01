d87166b4b69690748765fb97ac383d852a9cc4e7
xuyc@sina.com <xuyc@sina.com>
2019-07-01 11:05:49 +0800

    modified:   async2.cpp

--- concurrency/async2.cpp
+++ concurrency/async2.cpp
@@ -14,30 +14,24 @@ template <typename Iterator, typename T>
 T async_sum(Iterator first, Iterator last, T init)
 {
   FUNC_HEAD();
-  /*
-The template function async runs the function f asynchronously
- (potentially in a separate thread which may be part of a thread pool) 
- and returns a std::future that will eventually hold the result of that function call.
 
- limitation: no rsource lock needed
-*/
-  unsigned long const length = std::distance(first, last);
+  int const length = std::distance(first, last);
   if (!length) // 1
     return init;
 
-  unsigned long const min_per_thread = 1000;
-  unsigned long const max_threads =
+  int const min_per_thread = 1000;
+  int const max_threads =
       (length + min_per_thread - 1) / min_per_thread; // 2
 
-  unsigned long const hardware_threads =
+  int const hardware_threads =
       std::thread::hardware_concurrency();
 
   cout << " hardware_threads: " << hardware_threads << endl;
-  unsigned long const num_threads = // 3
+  int const num_threads = // 3
       std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
 
   cout << " num_threads: " << num_threads << endl;
-  unsigned long const block_size = length / num_threads; // 4
+  int const block_size = length / num_threads; // 4
 
   std::vector<std::future<int>> threads(num_threads); // 5
 
@@ -49,7 +43,14 @@ The template function async runs the function f asynchronously
     {
       iter_end = last;
     }
-    threads[i] = async(accumulate<vector<int>::iterator, int>, iter, iter_end, 0);
+    /*
+The template function async runs the function f asynchronously
+ (potentially in a separate thread which may be part of a thread pool) 
+ and returns a std::future that will eventually hold the result of that function call.
+
+ limitation: no rsource lock needed
+*/
+    threads[i] = async(accumulate<Iterator, T>, iter, iter_end, 0);
     iter = iter_end;
   }
   T sum = 0;
@@ -63,7 +64,7 @@ The template function async runs the function f asynchronously
 int main()
 {
   vector<int> v(1000000, 1);
-  unsigned long init = 0;
+  int init = 0;
   auto sum = async_sum(v.begin(), v.end(), init);
   cout << "\n\nThe sum =" << sum << endl;
 }
