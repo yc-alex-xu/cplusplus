f185a472e6904f00458035eef507c19fe7ede8ce
alex <xuyc@sina.com>
2019-08-08 11:35:36 +0800

    new file:   basic/toolkit.h

---
+++ basic/toolkit.h
@@ -0,0 +1,6 @@
+#ifndef TOOLKIT_H
+#define TOOLKIT_H
+#define FUNC_HEAD() cout << "-----------" << __FILE__ << "::" << __func__ << "-----------" << endl
+#define LINE_NO() cout << __LINE__ << ":"
+
+#endif
