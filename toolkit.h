05cabee25a3ae68e4b432a77fb720fa533d0e7d0
Alex Xu <xuyc@sina.com>
2019-09-10 17:13:32 +0800

    modified:   toolkit.h

--- toolkit.h
+++ toolkit.h
@@ -3,4 +3,19 @@
 #define FUNC_HEAD() cout << "-----------" << __FILE__ << "::" << __func__ << "-----------" << endl
 #define LINE_NO() cout << __LINE__ << ":"
 
+
+void update_progress_tobe(unsigned long lineno )
+{
+    static unsigned int count = 1;
+    char spin[]="|/-\\";
+    if (lineno > count * 10000000)
+    {
+        if (count == 1)
+            cout << endl
+                 << "progress: "; //cerr is not buffered
+        cout << '\b' << spin[count%4];
+        count++;
+    }
+}
+
 #endif
