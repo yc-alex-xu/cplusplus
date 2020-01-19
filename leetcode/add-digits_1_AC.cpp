5f87d6f4aebfa5cf0e80388e358cea1e80ef52f8
Alex Xu <xuyc@sina.com>
2020-01-19 15:01:27 +0800

    deleted:    leetcode/01-matrix_1_AC.cpp

--- leetcode/add-digits_1_AC.cpp
+++
@@ -1,7 +0,0 @@
-// Digital root.
-class Solution {
-public:
-    int addDigits(int num) {
-        return num > 0 ? (num + 8) % 9 + 1 : 0;
-    }
-};
--- practice/toolkit.h
+++
@@ -1,6 +0,0 @@
-#ifndef TOOLKIT_H
-#define TOOLKIT_H
-#define FUNC_HEAD() cout << "-----------" << __FILE__ << "::" << __func__ << "-----------" << endl
-#define LINE_NO() cout << __LINE__ << ":"
-
-#endif
